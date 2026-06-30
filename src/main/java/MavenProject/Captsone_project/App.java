cfec2e66ba8c5b3b6411f766a437f7238073e5cc
Balantrapu Sreevishnu <ba20695783@wipro.com>
2026-06-30 11:29:22 +0530

    Initial Capstone Project Commit

---
+++ src/main/java/MavenProject/Captsone_project/App.java
@@ -0,0 +1,10 @@
+package MavenProject.Captsone_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
