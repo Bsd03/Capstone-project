4bbcd07a77db5c633b8e6d1664696737c340aab8
rakeshroshan26 <rakeshroshan26@gmail.com>
2026-06-30 12:15:57 +0530

    Code in LoginPage

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,5 +1,38 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
 
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    By emailField = By.xpath("//input[@data-qa='login-email']");
+    By passwordField = By.xpath("//input[@data-qa='login-password']");
+    By loginButton = By.xpath("//button[@data-qa='login-button']");
+
+
+    public void enterEmail(String email) {
+        driver.findElement(emailField).sendKeys(email);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+    public void login(String email, String password) {
+        enterEmail(email);
+        
+        enterPassword(password);
+        
+        clickLogin();
+    }
 }
