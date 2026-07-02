09ab4a7d011767eb4a4ba682e52b97b29b06f8f5
Sayani Maity <sayanimaity03@gmail.com>
2026-07-02 12:51:07 +0530

    added Home Feature File , Brand Step def , Contact details Feature

---
+++ src/test/java/StepDefinitions/BrandStep.java
@@ -0,0 +1,32 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class BrandStep {
+
+    @When("User scrolls to Brands section")
+    public void user_scrolls_to_brands_section() {
+
+    }
+
+    @Then("Brands section should be visible")
+    public void brands_section_should_be_visible() {
+
+    }
+
+    @Then("List of brands should be displayed")
+    public void list_of_brands_should_be_displayed() {
+
+    }
+
+    @When("User clicks on {string} brand")
+    public void user_clicks_on_brand(String brandName) {
+
+    }
+
+    @Then("{word} brand products should be displayed")
+    public void brand_products_should_be_displayed(String brandName) {
+
+    }
+}
