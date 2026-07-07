46b2bfc0cc68a3fd847b6f7eecca19865e4f52eb
Afrin950-shaik <shaikafrin676@gmail.com>
2026-07-07 12:48:36 +0530

    updated allurereport

--- src/test/java/runner/runner.java
+++ src/test/java/runner/runner.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/Features/HomePage.feature",
 				glue={"StepDefinitions","Hooks"},
-				plugin= {"pretty","html:target/cucumber-reports/CucumberReport.html"}
+				plugin= {"pretty","html:target/cucumber-reports/CucumberReport.html",
+						"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
 	)
 
 public class runner extends AbstractTestNGCucumberTests {
