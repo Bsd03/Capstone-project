e2725618b434acab99a0cbe0585434c2df43d6ca
Afrin950-shaik <shaikafrin676@gmail.com>
2026-07-09 13:16:59 +0530

    Updated log4j

--- src/test/java/StepDefinitions/E_BrandStep.java
+++ src/test/java/StepDefinitions/E_BrandStep.java
@@ -1,26 +1,22 @@
 package StepDefinitions;
 
-import org.testng.Assert;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 
 import Hooks.Hooks;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.BrandsPage;
-import pages.ProductsPage;
 
 public class E_BrandStep {
-
-    private static final Logger logger = LogManager.getLogger(E_BrandStep.class);
-
+	private static final Logger logger=LogManager.getLogger(E_BrandStep.class);
     BrandsPage brandsPage = new BrandsPage(Hooks.driver);
-
     // Verify Brands Section
     @Then("the Brands section should be visible on the left side")
     public void the_brands_section_should_be_visible_on_the_left_side() {
-        logger.info("Verifying Brands section visibility");
+    		logger.info("Verifying Brands section visibility");
         brandsPage.verifyBrandsSection();
         logger.info("Brands section verified successfully");
     }
@@ -28,7 +24,7 @@ public class E_BrandStep {
     // Verify Brand Names
     @And("all available brand names should be displayed")
     public void all_available_brand_names_should_be_displayed() {
-        logger.info("Verifying all available brand names");
+    		logger.info("Verifying all available brand names");
         brandsPage.verifyBrandNames();
         logger.info("All brand names displayed successfully");
     }
@@ -36,23 +32,23 @@ public class E_BrandStep {
     // Click Any Brand
     @When("the user clicks on {string} brand")
     public void the_user_clicks_on_brand(String brandName) {
-        logger.info("Clicking on brand: {}", brandName);
+    		logger.info("Clicking on brand : {}",brandName);
         brandsPage.clickBrand(brandName);
-        logger.info("Successfully clicked brand: {}", brandName);
+        logger.info("Successfully clicked brand : {}",brandName);
     }
 
     // Verify Brand Page
     @Then("the {string} brand page should be displayed")
     public void the_brand_page_should_be_displayed(String brandName) {
-        logger.info("Verifying brand page for: {}", brandName);
+    		logger.info("Verifying brand page for: {}",brandName);
         brandsPage.verifyBrandPage(brandName);
-        logger.info("{} brand page displayed successfully", brandName);
+        logger.info("{} brand page displayed successfully",brandName);
     }
 
     // Verify Products Displayed
     @And("all displayed products should belong to {string}")
     public void all_displayed_products_should_belong_to(String brandName) {
-        logger.info("Verifying products displayed for brand: {}", brandName);
+    		logger.info("Verifying products displayed for brand: {}",brandName);
         brandsPage.verifyProductsDisplayed();
         logger.info("Products displayed successfully");
     }
@@ -60,15 +56,15 @@ public class E_BrandStep {
     // Verify Products after Switching Brands
     @Then("the {string} products should be displayed")
     public void the_products_should_be_displayed(String brandName) {
-        logger.info("Verifying switched brand products for: {}", brandName);
+    		logger.info("Verifying switched brand products for: {}",brandName);
         brandsPage.verifyProductsDisplayed();
-        logger.info("{} products displayed successfully", brandName);
+        logger.info("{} products displayed successfully",brandName);
     }
 
     // Verify URL
     @And("the URL should contain {string}")
     public void the_url_should_contain(String urlText) {
-        logger.info("Verifying URL contains: {}", urlText);
+    		logger.info("Verifying URL contains: {}",urlText);
         Assert.assertTrue(brandsPage.getCurrentPageURL().contains(urlText));
         logger.info("URL verification successful");
     }
@@ -76,7 +72,7 @@ public class E_BrandStep {
     // Open First Product
     @And("the user opens the first product")
     public void the_user_opens_the_first_product() {
-        logger.info("Opening first product");
+    		logger.info("Opening first product");
         brandsPage.openFirstProduct();
         logger.info("First product opened successfully");
     }
@@ -84,7 +80,7 @@ public class E_BrandStep {
     // Verify Product Name
     @Then("the Brands Page product name should be displayed")
     public void the_Brands_Page_product_name_should_be_displayed() {
-        logger.info("Verifying product name");
+    		logger.info("Verifying product name");
         brandsPage.verifyProductName();
         logger.info("Product name verified successfully");
     }
@@ -92,7 +88,7 @@ public class E_BrandStep {
     // Verify Product Category
     @And("the Brands Page product category should be displayed")
     public void the_Brands_Page_product_category_should_be_displayed() {
-        logger.info("Verifying product category");
+    		logger.info("Verifying product category");
         brandsPage.verifyCategory();
         logger.info("Product category verified successfully");
     }
@@ -100,7 +96,7 @@ public class E_BrandStep {
     // Verify Product Price
     @And("the Brands Page product price should be displayed")
     public void the_Brands_Page_product_price_should_be_displayed() {
-        logger.info("Verifying product price");
+    		logger.info("Verifying product price");
         brandsPage.verifyPrice();
         logger.info("Product price verified successfully");
     }
@@ -108,7 +104,7 @@ public class E_BrandStep {
     // Verify Product Availability
     @And("the Brands Page product availability should be displayed")
     public void the_Brands_Page_product_availability_should_be_displayed() {
-        logger.info("Verifying product availability");
+    		logger.info("Verifying product availability");
         brandsPage.verifyAvailability();
         logger.info("Product availability verified successfully");
     }
@@ -116,7 +112,7 @@ public class E_BrandStep {
     // Verify Product Brand
     @And("the Brands Page product brand should be {string}")
     public void the_Brands_Page_product_brand_should_be(String brandName) {
-        logger.info("Verifying product brand: {}", brandName);
+    		logger.info("Verifying product brand: {}",brandName);
         brandsPage.verifyBrand(brandName);
         logger.info("Product brand verified successfully");
     }
@@ -124,15 +120,15 @@ public class E_BrandStep {
     // Add Product to Cart
     @When("the user adds the first product to the cart")
     public void the_user_adds_the_first_product_to_the_cart() {
-        logger.info("Adding first product to cart");
+    		logger.info("Adding first product to cart");
         brandsPage.addFirstProductToCart();
         logger.info("Product added to cart successfully");
     }
 
     // Open Cart
     @When("User opens the cart from Brands page")
-    public void User_opens_the_cart_from_Brands_page() {
-        logger.info("Opening cart from Brands page");
+    public void  User_opens_the_cart_from_Brands_page() {
+    		logger.info("Opening cart from Brands page");
         brandsPage.openCart();
         logger.info("Cart opened successfully");
     }
@@ -140,7 +136,7 @@ public class E_BrandStep {
     // Verify Product in Cart
     @Then("the selected {string} product should be displayed in the cart")
     public void the_selected_product_should_be_displayed_in_the_cart(String brandName) {
-        logger.info("Verifying selected product in cart for brand: {}", brandName);
+    		logger.info("Verifying selected product in cart for brand: {}",brandName);
         brandsPage.verifyCartProduct();
         logger.info("Product verified successfully in cart");
     }
--- src/test/java/StepDefinitions/H_ContactUsSteps.java
+++ src/test/java/StepDefinitions/H_ContactUsSteps.java
@@ -1,5 +1,7 @@
 package StepDefinitions;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
@@ -10,54 +12,71 @@ import io.cucumber.java.en.When;
 import pages.ContactusPage;
 
 public class H_ContactUsSteps {
+	private static final Logger logger = LogManager.getLogger(H_ContactUsSteps.class);
 	WebDriver driver=Hooks.driver;
     ContactusPage contactUs = new ContactusPage(driver);
     
     @Given("User launches browser")
     public void User_launches_browser(){
-    	
+    		
+    		logger.info("User launches browser");
     }
 
     @Given("User navigates to the Automation Exercise website")
     public void user_navigates_to_the_automation_exercise_website() {
-
+    	
+    		logger.info("User navigates to Automation Exercise website");
     }
 
     @When("User clicks Contact Us menu")
     public void user_clicks_contact_us_menu() {
+    		logger.info("Clicking Contact Us menu");
         contactUs.clickContactUs();
+        logger.info("Contact Us menu clicked successfully");
     }
 
     @Then("Contact Us page should be displayed")
     public void contact_us_page_should_be_displayed() {
+    		logger.info("Verifying Contact Us page");
         Assert.assertTrue(contactUs.isContactPageDisplayed(),
                 "Contact Us page is not displayed");
+        logger.info("Contact Us page verified successfully");
     }
 
     @Then("Get In Touch section should be visible")
     public void get_in_touch_section_should_be_visible() {
+    		logger.info("Verifying Get In Touch section");
         Assert.assertTrue(contactUs.isGetInTouchVisible(),
                 "Get In Touch section is not visible");
+        logger.info("Get In Touch section verified successfully");
     }
 
     @When("User enters name {string}")
     public void user_enters_name(String name) {
+    		logger.info("Entering Name : {}", name);
         contactUs.enterName(name);
+        logger.info("Name entered successfully");
     }
 
     @When("User enters email {string}")
     public void user_enters_email(String email) {
+    		logger.info("Entering Email : {}", email);
         contactUs.enterEmail(email);
+        logger.info("Email entered successfully");
     }
 
     @When("User enters subject {string}")
     public void user_enters_subject(String subject) {
+    		logger.info("Entering Subject : {}", subject);
         contactUs.enterSubject(subject);
+        logger.info("Subject entered successfully");
     }
 
     @When("User enters message {string}")
     public void user_enters_message(String message) {
+    		logger.info("Entering Message");
         contactUs.enterMessage(message);
+        logger.info("Message entered successfully");
     }
 
     @When("User uploads file")
@@ -65,37 +84,41 @@ public class H_ContactUsSteps {
 
         String filePath = System.getProperty("user.dir")
                 + "\\src\\test\\resources\\testdata\\sample.txt";
-
+        logger.info("Uploading File : {}", filePath);
         contactUs.uploadFile(filePath);
+        logger.info("File uploaded successfully");
     }
 
     @When("User clicks Submit button")
     public void user_clicks_submit_button() {
-
+    		logger.info("Clicking Submit button");
         contactUs.clickSubmit();
-
+        logger.info("Handling confirmation alert");
         // Handle confirmation alert
         contactUs.acceptAlert();
     }
 
     @Then("Success message should be displayed")
     public void success_message_should_be_displayed() {
-
+    		logger.info("Verifying Success Message");
         Assert.assertTrue(contactUs.isSuccessMessageDisplayed(),
                 "Success message is not displayed");
+        logger.info("Success message verified successfully");
     }
 
     @Then("Feedback For Us section should be displayed")
     public void feedback_for_us_section_should_be_displayed() {
-
+    		logger.info("Verifying Feedback For Us section");
         Assert.assertTrue(contactUs.isFeedbackisDisplayed(),
                 "Feedback For Us section is not displayed");
+        logger.info("Feedback For Us section verified successfully");
     }
 
     @Then("Feedback email should be visible")
     public void feedback_email_should_be_visible() {
-
+    		logger.info("Verifying Feedback email");
         Assert.assertTrue(contactUs.isFeedbackEmailDisplayed(),
                 "Feedback email is not displayed");
+        logger.info("Feedback email verified successfully");
     }
 }
